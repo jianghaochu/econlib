4e3cd7efe32e61501cd12bfcbd602a26aa1b4f94
Jianghao Chu <31344153+jianghaochu@users.noreply.github.com>
2020-04-23 09:58:52 -0400

    Set up Docs using Sphinx

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="econlib",
-    version="0.0.1",
+    version="0.0.2",
     author="Jianghao Chu",
     author_email="jianghaochu@gmail.com",
     description="A package for econometric models and methods",
