fa92a4381cff3233d3de287423dc36592a029012
Jianghao Chu <31344153+jianghaochu@users.noreply.github.com>
2020-04-28 22:33:29 -0400

    Added Log-Likelihood, AIC, BIC Changed summary to output string Changed summary output format

--- econlib/example_module.py
+++ econlib/example_module.py
@@ -1,62 +1,117 @@
-#!/usr/bin/python
+class ExampleClass(object):
+    """The summary line for a class docstring should fit on one line.
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. Alternatively, attributes may be documented
+    inline with the attribute's declaration (see __init__ method below).
 
-"""
-This is an example Python module for the Python to Read the Docs
-documentation repository.
-It is used to show how Sphinx autodoc can be used to auto-generate
-Python documentation from doc strings like this...
-Written by Matthew John Hayes
-"""
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
 
-CONSTANT = 'example constant value'
+    Parameters
+    ----------
+    param1 : str
+        Description of `param1`.
+    param2 : :obj:`list` of :obj:`str`
+        Description of `param2`. Multiple
+        lines are supported.
+    param3 : :obj:`int`, optional
+        Description of `param3`.
 
-class ExampleModule(object):
+    Attributes
+    ----------
+    attr1 : str
+        Description of `attr1`.
+    attr2 : :obj:`int`, optional
+        Description of `attr2`.
     """
-    This is the main class of example_module. It doesn't do anything useful
-    other than show how classes are documented by autodoc
-    """
-    def __init__(self):
-        """
-        Initialise the ExampleModule class
-        """
-        self.class_variable = 1
 
-    def run(self):
-        """
-        Run the ExampleModule instance
-        """
-        self.increment(5)
-        print("Class Variable is", self.class_variable)
+    def __init__(self, param1, param2, param3):
+        self.attr1 = param1
+        self.attr2 = param2
+        self.attr3 = param3  #: Doc comment *inline* with attribute
 
-    def increment(self, value):
-        """
-        Increment the value of self.class_variable by value passed
-        to this method
-        """
-        self.class_variable += value
+        #: list of str: Doc comment *before* attribute, with type specified
+        self.attr4 = ["attr4"]
 
-    def _private_method(self):
-        """
-        Example private method that won't be documented by autodoc
-        unless you add :private-members: to the automodule directive
-        """
-        self.class_variable += 1
+        self.attr5 = None
+        """str: Docstring *after* attribute, with type specified."""
 
-if __name__ == '__main__':
-    #*** Instantiate the ExampleModule class:
-    example_module = ExampleModule()
-    #*** Start ExampleModule:
-    example_module.run()
+    @property
+    def readonly_property(self):
+        """str: Properties should be documented in their getter method."""
+        return "readonly_property"
+
+    @property
+    def readwrite_property(self):
+        """:obj:`list` of :obj:`str`: Properties with both a getter and setter
+        should only be documented in their getter method.
+
+        If the setter method contains notable behavior, it should be
+        mentioned here.
+        """
+        return ["readwrite_property"]
+
+    @readwrite_property.setter
+    def readwrite_property(self, value):
+        value
+
+    def example_method(self, param1, param2):
+        """Class methods are similar to regular functions.
+
+        Note
+        ----
+        Do not include the `self` parameter in the ``Parameters`` section.
+
+        Parameters
+        ----------
+        param1
+            The first parameter.
+        param2
+            The second parameter.
+
+        Returns
+        -------
+        bool
+            True if successful, False otherwise.
+
+        """
+        return True
+
+    def __special__(self):
+        """By default special members with docstrings are not included.
+
+        Special members are any methods or attributes that start with and
+        end with a double underscore. Any special member with a docstring
+        will be included in the output, if
+        ``napoleon_include_special_with_doc`` is set to True.
+
+        This behavior can be enabled by changing the following setting in
+        Sphinx's conf.py::
+
+            napoleon_include_special_with_doc = True
+
+        """
+        pass
+
+    def __special_without_docstring__(self):
+        pass
+
+    def _private(self):
+        """By default private members are not included.
+
+        Private members are any methods or attributes that start with an
+        underscore and are *not* special. By default they are not included
+        in the output.
+
+        This behavior can be changed such that private members *are* included
+        by changing the following setting in Sphinx's conf.py::
+
+            napoleon_include_private_with_doc = True
+
+        """
+        pass
+
+    def _private_without_docstring(self):
+        pass
